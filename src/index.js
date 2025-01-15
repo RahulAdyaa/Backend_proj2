585a1a100e1ede8fb2db7bb565faf6f4bc3ec267
Rahul adya <radya_be22@thapar.edu>
2025-01-15 23:37:40 +0530

    setup stage-2 done

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+//always start the IIFE(IMMEDIATELY INVOKED FUNCTION EXPRESSIONS) with a semicolon
+connectDB()
+
+
+dotenv.config({
+    path:'./env'
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import express from "express";
+const app=express()
+
+(async () => {
+  try {
+    //connecting database
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    //listeners
+    app.on("error",(error)=>{
+        console.log("ERROR",error)
+        throw error
+    })
+
+    app.listen(process.env.PORT,()=>{
+        console.log(`App is listening on port ${process.env.PORT}`)
+    })
+  } catch (error) {
+    console.log("ERROR", error);
+    throw error;
+  }
+})();*/
