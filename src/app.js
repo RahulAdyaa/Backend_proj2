a7cfbc86f950255d3caa5e766621aec32df61c65
Rahul adya <radya_be22@thapar.edu>
2025-01-16 15:44:20 +0530

    utils code discussion

--- src/app.js
+++ src/app.js
@@ -13,5 +13,6 @@ app.use(cors({
 
 app.use(express.json({limit:"16kb"})) // for accepeting json
 app.use(express.urlencoded({extended:true , limit:"16kb"}))
-app.use(express.static("public")) // to store public assets
+app.use(express.static("public")) // to store public assets , we have already made public folder
+app.use(cookiePareser())
 export { app }
