875029f086ad0c11725dba1350cf8ff920a2b74e
Rahul adya <radya_be22@thapar.edu>
2025-01-23 16:40:00 +0530

    Add getwatchHistory endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,6 +6,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
@@ -371,63 +372,116 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
 
   const channel = await User.aggregate([
     {
-    $match: {
-      username: username?.toLowerCase(),
+      $match: {
+        username: username?.toLowerCase(),
       },
     },
     {
-      $lookup:{
-        from :"subscriptions",
+      $lookup: {
+        from: "subscriptions",
         localField: "_id",
         foreignField: "channel",
         as: "subscribers",
-      }
+      },
     },
     {
-      $lookup:{
-        from :"subscriptions",
+      $lookup: {
+        from: "subscriptions",
         localField: "_id",
         foreignField: "channel",
         as: "subscribedTo",
-      }
+      },
     },
     {
-      $addFields:{
-        subscribersCount:{
-          $size:"$subscribers"
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers",
         },
-        channelSubscribedToCount:{
-          $size:"$subscribedTo"
+        channelSubscribedToCount: {
+          $size: "$subscribedTo",
         },
-        isubscribed:{
-          $cond:{
-            if:{
-              $in:[req.user?._id,"$subscribers.subscriber"]
+        isubscribed: {
+          $cond: {
+            if: {
+              $in: [req.user?._id, "$subscribers.subscriber"],
             },
-            then:true,
-            else:false
-          }
-        }
-      }
+            then: true,
+            else: false,
+          },
+        },
+      },
     },
     {
-      $project:{  
+      $project: {
         fullName: 1,
         username: 1,
         subscribersCount: 1,
         channelSubscribedToCount: 1,
         isubscribed: 1,
-        avatar:1,
-        coverImage:1,
-        email:1
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+      },
+    },
+  ]);
+  console.log(channel);
+
+  if (!channel?.length) {
+    throw new ApiError(404, "channel does not exist");
+  }
+
+  return res
+  .status(200)
+  .json(new ApiError(200,channel[0],"User channel data fetched successfully"))
+});
+
+const getWatchHistory = asyncHandler(async(req,res)=>{
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user?._id),
+      }
+    },
+    {
+      $lookup: {
+        from :"videos",
+        localField:"watchHistory",
+        foreignField:"_id",
+        as:"watchHistory",
+        pipeline:[
+          {
+            $lookup:{
+              from :"users",
+              localField:"owner",
+              foreignField:"_id",
+              as:"owner",
+              pipeline:{
+                $project:{
+                  username:1,
+                  avatar:1,
+                  fullName:1
+                }
+              }
+            }
+          },
+          {
+            $addFields:{
+              owner:{
+                $first:"$owner"
+              }
+            }
+          }
+        ]
 
       }
     }
-
-    
-]);
-console.log(channel)
-});
+  ])
+  return res
+  .status(200)
+  .json(
+    new ApiError(200,user[0].watchHistory,"User watch history data fetched successfully")
+  )
+})
 
 export {
   registerUser,
@@ -440,6 +494,7 @@ export {
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannelProfile,
+  getWatchHistory
 };
 
 // if we are using req , and next but not res , then we can write like
